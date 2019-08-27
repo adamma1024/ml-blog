72348a6b345916466dc090cac1223fadbc6bf6d2
qq240814476 <240814476@qq.com>
2019-08-28 07:08:08 +0800

    a

--- sw.js
+++ sw.js
@@ -54,7 +54,6 @@ const getCacheBustingUrl = (req) => {
   // max-age on mutable content is error-prone, with SW life of bugs can even extend.
   // Until cache mode of Fetch API landed, we have to workaround cache-busting with query string.
   // Cache-Control-Bug: https://bugs.chromium.org/p/chromium/issues/detail?id=453190
-  url.search += (url.search ? '&' : '?') + 'cache-bust=' + now;
   return url.href
 }
 
