75a90d9035bb08a2d50abaa5045ce81af2ea54f5
qq240814476 <240814476@qq.com>
2019-08-28 19:27:01 +0800

    a

--- sw.js
+++ sw.js
@@ -31,8 +31,8 @@ const PRECACHE_LIST = [
 ]
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
-  "huangxuan.me",
-  "yanshuo.io",
+  "malin-life.com",
+  "185.199.111.153",
   "cdnjs.cloudflare.com"
 ]
 const DEPRECATED_CACHES = ['precache-v1', 'runtime', 'main-precache-v1', 'main-runtime']
