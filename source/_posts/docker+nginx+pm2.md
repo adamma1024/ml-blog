4cd86acf2e7eb025efd16c98ab31fd83befb7784
qq240814476 <240814476@qq.com>
2020-03-03 00:04:44 +0800

    docker

---
+++ source/_posts/docker+nginx+pm2.md
@@ -0,0 +1,2 @@
+我执行了这个命令修改了密钥的格式才能被Deploy解析
+ssh-keygen -p -m PEM -f ~/.ssh/id_rsa
---
+++ webhook.js
@@ -0,0 +1,40 @@
+const http = require('http')
+const createHandler = require('github-webhook-handler')
+
+const handler = createHandler({
+    path:'/docker_deploy',
+    secret:'malin258'
+})
+
+function run_cmd(cmd, args, callback) {
+  var spawn = require('child_process').spawn;
+  var child = spawn(cmd, args);
+  var resp = "";
+  child.stdout.on('data', function (buffer) {
+    resp += buffer.toString();
+  });
+  child.stdout.on('end', function () {
+    callback(resp)
+  });
+}
+
+http.createServer((req,res) => {
+  handler(req,res,err => { res.statusCode = 404
+    res.end('no such location')
+  })
+}).listen(7777, () => {
+  console.log('Webhook listen at 7777')
+})
+
+handler.on('error',err => {
+  console.error('Error',err.message)
+})
+
+handler.on('push', function (event) {
+  console.log('Received a push event for %s to %s', event.payload.repository.name, event.payload.ref);
+  // 分支判断
+  if(event.payload.ref === 'refs/heads/master'){
+    console.log('deploy master..')
+    run_cmd('sh', ['./push.sh'], function(text){ console.log(text) });
+  }
+})
