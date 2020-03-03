961fed7f90f4e0cce93bcc21ec00683055ba806d
qq240814476 <240814476@qq.com>
2020-03-03 22:56:36 +0800

    docker

--- pull.sh
+++ pull.sh
@@ -5,5 +5,5 @@ echo Deploy Project # 获取最新版代码
 git pull
 
 # 强制重新编译容器
-# docker-compose down
-# docker-compose up -d --force-recreate --build
+docker-compose down
+docker-compose up -d --force-recreate --build
